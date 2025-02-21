b82c792f14c717a3babfb043f5f6e6b70a419294
Shivam Kumar Jha <shkrjh@gmail.com>
2025-02-21 21:58:53 +0530

    [Shivam Kumar Jha] Add . Rest controller using POST method and using DTO Bean

--- src/main/java/RESTApi/Controller/AppController.java
+++ src/main/java/RESTApi/Controller/AppController.java
@@ -1,9 +1,6 @@
 package RESTApi.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AppController {
@@ -20,5 +17,10 @@ public class AppController {
     @GetMapping("/hello/{name}")
     public String getResponse(@PathVariable String name) {
         return name + " from BridgeLabz";
+    }
+
+    @PostMapping("/hello/post")
+    public String sayHelloWithBody(@RequestBody User user) {
+        return "Hello " + user.getFirstName() + "\n" + user.getLastName() + " from BridgeLabz";
     }
 }
---
+++ src/main/java/RESTApi/Controller/User.java
@@ -0,0 +1,25 @@
+package RESTApi.Controller;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class User {
+    @JsonProperty
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
